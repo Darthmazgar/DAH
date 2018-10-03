4f82a9d51ec2d2080d8514939008b629b8af4f63
Iain Mclaughlan <34077975+Darthmazgar@users.noreply.github.com>
2018-10-03 14:33:55 +0100

    More changes

--- scripts/checkpoint_3b.py
+++ scripts/checkpoint_3b.py
@@ -1,4 +1,6 @@
 from webiopi.devices.analog.mcp3x0x import MCP3208  # ADC Lib
+from webiopi.devices.analog.mcp492X import MCP492X  # DAC lib
+
 import pylab as plt
 import numpy as np
 import time
@@ -28,12 +30,12 @@ def avg_data(data):
     return mean, high_mean, low_mean
 
 def alt_avg_data(data):
-    max = np.amax()
-    mid = max / 2
+    amax = np.amax()
+    mid = amax / 2
     high = []
     low = []
     for point in data:
-        if point > mean:
+        if point > mid:
             high.append(point)
         else:
             low.append(point)
@@ -41,8 +43,20 @@ def alt_avg_data(data):
     low_mean = np.mean(low)
     return mid, high_mean, low_mean
 
-def send_data():
-    pass    
+def send_data(data, maxv, chip):
+    if np.amax(data) > maxv:
+        # print("Fucking abort, Bro!")
+        data = crop_data(data, maxv)
+    for i in data:
+        chip.analogWriteVolt(0, i)
+        
+def crop_data(data, maxv):
+    for point in data: 
+        if point > maxv:
+            point = maxv
+        if point < -maxv:
+            point = -maxv
+    return data
 
 def plot_time_data(y_data, sample_no, run_time, x_lab="", y_lab="", title="" show=True):
     x_data = [x * 10 *run_time / sample_no for x in range(sample_no)]
@@ -61,11 +75,13 @@ def write_to_file(data, sample_no, run_time, file="cp3c.txt", info=""):
         out_file.write("Time: %f, Voltage: %f" % (time_step * (i+1), data[i]))
     out_file.close() 
     
-def sin_wave(no_steps, amp):
-    retun [amp*np.sin(x) for x in range(no_steps)
+def sin_wave(no_steps, amp, dt):
+    retun [amp*np.sin(x*dt) for x in range(no_steps)]
            
 def main():
     ADC0 = MCP3208(chip=0)
+    DAC1 = MCP492X(chip=1, channelCount=2, vref=3.3)
+
     sample_no = 100
 
     data, run_time = read_data(sample_no, chip=ADC0)
