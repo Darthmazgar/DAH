128edb9e6dc3210420ec951667e579c6ce24b0e8
Iain Mclaughlan <34077975+Darthmazgar@users.noreply.github.com>
2018-10-10 14:04:57 +0100

    Blinking lights.
    Turn on LEDs in order and then of once completed in order.

--- scripts/checkpoint_4b.py
+++ scripts/checkpoint_4b.py
@@ -1,8 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+import numpy as np
+from webiopi.devices.digital.pcf8574
+
+mcp = PCF8574A(slave=0x38)  # decimal 56
+
+num_led = 3
+LED_arr = np.arange(num_led)  # Set arr components as 0, 1, 2 
+
+for i in range(num_led):
+    mcp.setFunction(LED_arr[i], GPIO.OUT)  # Set pin as an output
+    
 # Loop for ever
 while True:
-    # Read the switch and if it is pressed toggle the state of the LED
-    if (mcp.digitalRead(SWITCH0) == GPIO.LOW):
-        # Insert your code here
-
-        # dummy write to reset switch register
-        mcp.digitalWrite(SWITCH0, GPIO.HIGH)
+    for led in LED_arr:
+        value = not GPIO.input(led)
+        GPIO.output(LED0, value)
+        time.sleep(0.10)
