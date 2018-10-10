05c924e18d91a66d2ab8494571664f723d8dd0ec
Iain Mclaughlan <34077975+Darthmazgar@users.noreply.github.com>
2018-10-10 14:26:28 +0100

    Added possible solution
    unsure about how to register if a button has been pressed.

--- scripts/checkpoint_4c.py
+++ scripts/checkpoint_4c.py
@@ -12,21 +12,23 @@ for i in range(num_led):
     mcp.setFunction(LED_arr[i], GPIO.OUT)  # Set pin as an output
     
 # Loop for ever
-value = 1
+value = 0
 while True:
     if(mcp.digitalRead(SWITCH0) == GPIO.LOW):
         for led in LED_arr:
             value = not GPIO.input(led)
             GPIO.output(LED0, value)
             time.sleep(0.10)
-        
+
+    else:
+        portWrite(value)
+        """
+        if value == 1:
+            value = 0
         else:
-            portWrite(value)
-            if value == 1:
-                value = 0
-            else:
-                value = 1
-        mcp.digitalWrite(SWITCH0, GPIO.HIGH)
+            value = 1
+        """"
+    mcp.digitalWrite(SWITCH0, GPIO.HIGH)
 """
     if button_pushed:
         
