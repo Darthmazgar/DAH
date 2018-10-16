733aabe877098cb0c966a740a38c6d7bc94c46ee
Darthmazgar <student04@pipsmthree>
2018-10-16 16:59:03 +0100

    CP4c complete

--- scripts/checkpoint_4c.py
+++ scripts/checkpoint_4c.py
@@ -1,3 +1,7 @@
+"""
+Intermitant issues with loop not restarting.
+"""
+
 import RPi.GPIO as GPIO
 import time
 import numpy as np
@@ -10,24 +14,28 @@ LED_arr = np.arange(num_led)  # Set arr components as 0, 1, 2
 SWITCH0 = mcp.setFunction(4, GPIO.IN)
 
 for i in range(num_led):
-    print(i)
     mcp.setFunction(LED_arr[i], GPIO.OUT)  # Set pin as an output
 
 
 # Loop for ever
 while True:
-    if (mcp.digitalRead(4) == GPIO.HIGH):
-        print("LOW")
-        # mcp.digitalWrite(SWITCH0, GPIO.LOW)
-    for led in LED_arr:
+    if (mcp.digitalRead(4) == GPIO.LOW):  # Button pressed
+        mcp.portWrite(15)  # Turn all LEDs of
+        loop = True
+        while loop:  # Wait for button to be pressed again
+            time.sleep(0.2)  # Allow for finger to release from button
+            mcp.digitalWrite(4, GPIO.HIGH)
+            if (mcp.digitalRead(4) == GPIO.LOW):  # Button pressed again so start looping again
+                loop = False
+                time.sleep(0.2)  # Allow for finger to release from button
+        mcp.digitalWrite(4, GPIO.HIGH)
+            
+    
+    for led in LED_arr:  # LED display
         value = not mcp.digitalRead(led)
-        # GPIO.output(LED0, value)
         mcp.digitalWrite(led, value)
         time.sleep(0.25)
+        
 
-    #else:
-        #mcp.portWrite(value)  # Probs wrong
-    
-    # mcp.digitalWrite(SWITCH0, GPIO.HIGH)
        
    
