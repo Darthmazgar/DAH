b1a62ae704e61788384a52ba77af0df57307e7e3
Iain Mclaughlan <34077975+Darthmazgar@users.noreply.github.com>
2018-10-18 15:37:42 +0100

    Define temp sensor and add it to colect info; plot

--- scripts/checkpoint_5b.py
+++ scripts/checkpoint_5b.py
@@ -1,6 +1,9 @@
 import pylab
 import matplotlib.animation as animation
 import datetime
+from webiopi.devices.sensor.onewiretemp impor DS18S20
+
+tmp0 = DS18S20(slave="10-00080265b6d6")  # Define temp sensor
 
 # Empty arrays of time and measurement values to plot
 timeValues = [ ]
@@ -13,7 +16,7 @@ plotFigure = pylab.figure()
 def updatePlot( i ):
 
     timeValues.append( datetime.datetime.now() ) # Store the current time
-    measurements.append( MEASUREMENT )           # Store the measurement
+    measurements.append( tmp0.getCelsius )           # Store the measurement
     plotFigure.clear()                           # Clear the old plot
     pylab.plot( timeValues, measurements )       # Make the new plot
 
