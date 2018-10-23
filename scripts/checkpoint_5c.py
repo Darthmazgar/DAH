01087c97501e82c9e1341f31446a128df3ebe661
Darthmazgar <student04@pipsmthree>
2018-10-23 15:58:42 +0100

    final save

--- scripts/checkpoint_5c.py
+++ scripts/checkpoint_5c.py
@@ -3,6 +3,8 @@ from webiopi.devices.sensor.onewiretemp import DS18S20
 import pylab
 import matplotlib.animation as animation
 import datetime
+import numpy as np
+
 # Readout temperature sensor
 tmp0 = DS18S20(slave="10-000802ddf685")
 tmp1 = DS18S20(slave="10-000802deb0fc")
@@ -45,12 +47,25 @@ def plot_50(size=50):
 def hist_50(size=50):
     if len(measurements1) < size:
         print("Too few data for full histogram plot.")
-    pylab.hist(measurements1[:size], bins=50)
-    pylab.hist(measurements2[:size], bins=50)
+    pylab.hist(measurements1[:size], bins=15)
+    pylab.hist(measurements2[:size], bins=15)
 
     pylab.ylabel("Frequency")
     pylab.xlabel("Temperature ($^oC$)")
     pylab.title("Temperature Frequency Histogram")
+    pylab.show()
+
+def delta():
+    d = []
+    for i in range(len(measurements1)):
+        d.append(measurements2[i] - measurements1[i])
+    return d
+
+def plot_diff(data, time):
+    pylab.plot(data, time)
+    pylab.ylabel("Temperature Difference ($^oC$)")
+    pylab.xlabel("Time")
+    pylab.title("Temperature Difference Between Sensors with Time")
     pylab.show()
 
 # Make the animated plot
@@ -59,3 +74,8 @@ pylab.show()
 
 plot_50()
 hist_50()
+delta_d = delta()
+plot_diff(timeValues, delta_d)
+rms = np.sqrt(np.mean(np.square(delta_d)))
+
+print("RMS: %.2f" % rms)
