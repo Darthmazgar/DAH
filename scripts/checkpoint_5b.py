c4f2309c6fbdc41bcdc1ceceea701b602e2e5816
Darthmazgar <student04@pipsmthree>
2018-10-23 15:18:24 +0100

    5.1 & 5.2 complete

--- scripts/checkpoint_5b.py
+++ scripts/checkpoint_5b.py
@@ -1,9 +1,9 @@
 import pylab
 import matplotlib.animation as animation
 import datetime
-from webiopi.devices.sensor.onewiretemp impor DS18S20
+from webiopi.devices.sensor.onewiretemp import DS18S20
 
-tmp0 = DS18S20(slave="10-00080265b6d6")  # Define temp sensor
+tmp0 = DS18S20(slave="10-000802deb0fc")  # Define temp sensor
 
 # Empty arrays of time and measurement values to plot
 timeValues = [ ]
@@ -11,16 +11,30 @@ measurements = [ ]
 
 # Set up the plot object
 plotFigure = pylab.figure()
+# plotFigure.title("Temperature Varying with Time (Live)")
 
 # The function to call each time the plot is updated
 def updatePlot( i ):
 
     timeValues.append( datetime.datetime.now() ) # Store the current time
-    measurements.append( tmp0.getCelsius )           # Store the measurement
+    measurements.append( tmp0.getCelsius() )           # Store the measurement
     plotFigure.clear()                           # Clear the old plot
+    pylab.ylabel("Temperature ($^oC$)")
+    pylab.xlabel("Time")
+    pylab.title("Temperature Varying With Time (Live Update)")
     pylab.plot( timeValues, measurements )       # Make the new plot
 
+def plot_50(size=50):
+    if len(measurements) < size:
+        print("Too few data for for full plot.")
+    pylab.plot(timeValues[:size], measurements[:size])
+    pylab.ylabel("Temperature ($^oC$)")
+    pylab.xlabel("Time")
+    pylab.title("Temperature Varying With Time")
+    pylab.show()
 
 # Make the animated plot
-ani = animation.FuncAnimation( plotFigure, updatePlot, interval=1000 )
+ani = animation.FuncAnimation( plotFigure, updatePlot, interval=500 )
 pylab.show()
+
+plot_50()
--- scripts/checkpoint_5c.py
+++ scripts/checkpoint_5c.py
@@ -1,7 +1,61 @@
 # Import
 from webiopi.devices.sensor.onewiretemp import DS18S20
-
+import pylab
+import matplotlib.animation as animation
+import datetime
 # Readout temperature sensor
-tmp0 = DS18S20(slave="10-00080265b6d6")
-tmp1 = DS18S20(slave="10-000802cb3b5c")
-print ( str( tmp0.getCelsius() ) + ", " + str( tmp1.getCelsius() ) )
+tmp0 = DS18S20(slave="10-000802ddf685")
+tmp1 = DS18S20(slave="10-000802deb0fc")
+# print ( str( tmp0.getCelsius() ) + ", " + str( tmp1.getCelsius() ) )
+
+timeValues = [ ]
+measurements1 = [ ]
+measurements2 = [ ]
+
+
+# Set up the plot object
+plotFigure = pylab.figure()
+# plotFigure.title("Temperature Varying with Time (Live)")
+
+# The function to call each time the plot is updated
+def updatePlot( i ):
+
+    timeValues.append( datetime.datetime.now() ) # Store the current time
+    measurements1.append( tmp0.getCelsius() )           # Store the measurement
+    measurements2.append( tmp1.getCelsius() ) 
+    plotFigure.clear()                           # Clear the old plot
+    pylab.ylabel("Temperature ($^oC$)")
+    pylab.xlabel("Time")
+    pylab.title("Temperature Varying With Time (Live Update)")
+    pylab.plot( timeValues, measurements1 )
+    pylab.plot( timeValues, measurements2 )       
+
+
+def plot_50(size=50):
+    if len(measurements1) < size:
+        print("Too few data for full plot.")
+    pylab.plot(timeValues[:size], measurements1[:size])
+    pylab.plot(timeValues[:size], measurements2[:size])
+
+    pylab.ylabel("Temperature ($^oC$)")
+    pylab.xlabel("Time")
+    pylab.title("Temperature Varying With Time")
+    pylab.show()
+
+def hist_50(size=50):
+    if len(measurements1) < size:
+        print("Too few data for full histogram plot.")
+    pylab.hist(measurements1[:size], bins=50)
+    pylab.hist(measurements2[:size], bins=50)
+
+    pylab.ylabel("Frequency")
+    pylab.xlabel("Temperature ($^oC$)")
+    pylab.title("Temperature Frequency Histogram")
+    pylab.show()
+
+# Make the animated plot
+ani = animation.FuncAnimation( plotFigure, updatePlot, interval=500 )
+pylab.show()
+
+plot_50()
+hist_50()
