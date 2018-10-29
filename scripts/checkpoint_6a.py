bbcab2d588f50c2fa5c9023c8dcddcfc1360a42c
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2018-10-29 16:24:42 +0000

    Tidy up

--- scripts/checkpoint_6a.py
+++ scripts/checkpoint_6a.py
@@ -71,7 +71,7 @@ def find_peak(x, y, smoothing=3, percentage_peak=15, show_peaks=True, save=False
 
 def peak_analysis(y_data, x_data, peak_index, disp_width=True):
     ranges = []
-    indicies = []
+    indices = []
     for peak in peak_index:
         ind = peak
         next_less = True
@@ -94,12 +94,12 @@ def peak_analysis(y_data, x_data, peak_index, disp_width=True):
                 prev_less = False
             ind -= 1
         ranges.append([x_data[left_min], x_data[right_min]])
-        indicies.append([left_min, right_min])
+        indices.append([left_min, right_min])
         if disp_width:
             plt.axvline(x_data[left_min], color='green', linewidth=.8)
             plt.axvline(x_data[right_min], color='green', linewidth=.8)
 
-    return ranges, indicies
+    return ranges, indices
 
 
 def region_mean(x_data, ind_range):
@@ -112,6 +112,10 @@ def region_variance(x_data, ind_range):
 
 def region_stdev(x_data, ind_range):
     return np.std(x_data[ind_range[0]:ind_range[1]])
+
+
+def std_err_mean(stdev, n):
+    return stdev / np.sqrt(n)
 
 
 def FWHM(peak_ind, ind_range, y_data, x_data, disp_half_width=True):
@@ -149,7 +153,7 @@ def FWHM(peak_ind, ind_range, y_data, x_data, disp_half_width=True):
 
 
 def est_sigma(fwhm):
-    return fwhm / 2.35
+    return fwhm / 2.355
 
 
 def main():
@@ -157,21 +161,25 @@ def main():
     n, bins, patches = plot_hist(data, title="Muon Pair Masses", x_lab="Mass ($GeV/c^2$)", y_lab="Frequency")
     peak_masses, peak_index = find_peak(x=bins[:-1], y=n)
     differences = peak_masses - peak_masses[0]
-    print("The muon pair masses are: %.3f, %.3f and %.3f GeV/c^2." % (peak_masses[0], peak_masses[1], peak_masses[2]))
+    print("The Muon pair masses are: %.3f, %.3f and %.3f GeV/c^2." % (peak_masses[0], peak_masses[1], peak_masses[2]))
     print("The Muon mass differences are: %.3f and %.3f GeV/c^2." %(differences[1], differences[2]))
     mass_ranges, index_ranges = peak_analysis(y_data=n, x_data=bins, peak_index=peak_index)  # peak_location
 
     peak_region_means = []
     region_var = []
     region_std = []
+    std_err = []
     for i in range(len(index_ranges)):
         peak_region_means.append(region_mean(x_data=bins, ind_range=index_ranges[i]))
         region_var.append(region_variance(x_data=bins, ind_range=index_ranges[i]))
         region_std.append(region_stdev(x_data=bins, ind_range=index_ranges[i]))
+        # n = index_ranges[i][1] - index_ranges[i][0]
+        # std_err.append(std_err_mean(region_std, n))
         # print(peak_region_means[i])
         # print(reigon_var[i])
         # print(reigon_std[i])
     # Still need to find out what std dev on the mean is?! Bias probs because left higher than right
+    # print(std_err[0])
 
     fwhm = FWHM(peak_index[0], index_ranges[0], n, bins)
     sigma = est_sigma(fwhm)
