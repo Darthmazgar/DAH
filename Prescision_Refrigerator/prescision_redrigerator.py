3f34ea46a0e53a9f9c61ebad30138a4ee26035b1
Darthmazgar <student04@pipsmthree>
2018-10-30 15:46:10 +0000

    Have therm iDs

--- Prescision_Refrigerator/prescision_redrigerator.py
+++ Prescision_Refrigerator/prescision_redrigerator.py
@@ -1,9 +1,16 @@
-from webiopi.devices.sensor.onewiretemp import DS18S20
+from webiopi.devices.sensor.onewiretemp import DS18B20
 import pylab
 import matplotlib.animation as animation
 import datetime
 import numpy as np
 
 # Readout temperature sensor
-tmp0 = DS18S20(slave="10-000802ddf685")
-tmp1 = DS18S20(slave="10-000802deb0fc")
+tmp0 = DS18B20(slave="28-000005e94da7")
+tmp1 = DS18B20(slave="28-000006cb82c6")
+print(tmp0.getCelsius())
+print(tmp1.getCelsius())
+
+class Cooler(object):
+    def __init__(self):
+        pass
+    
