f71b6a408050af7515bbca5ecf94b1f9b14fc6d9
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2018-10-30 16:18:15 +0000

    Tester and more prep

--- Prescision_Refrigerator/Thermometer.py
+++ Prescision_Refrigerator/Thermometer.py
@@ -1,6 +1,6 @@
 class Thermometer(object):
-    def __init__(self, adress):
-        self.therm = adress
+    def __init__(self, GPIO, address):
+        self.therm = address
 
     def cels_to_K(self, cels):
         return cels + 273
@@ -8,7 +8,7 @@ class Thermometer(object):
     def K_to_cels(self, k):
         return k - 273
 
-    def print_temp(self):
+    def print_tmp(self):
         tmp = self.therm.getCelsius()
         print(tmp)
         return tmp
--- Prescision_Refrigerator/main.py
+++ Prescision_Refrigerator/main.py
@@ -1,9 +1,9 @@
-# from webiopi.devices.sensor.onewiretemp import DS18B20
+from webiopi.devices.sensor.onewiretemp import DS18B20
 import pylab
 import matplotlib.animation as animation
 import datetime
 import numpy as np
-
+import RPi.GPIO as GPIO
 from Cooler import Cooler
 from Thermometer import Thermometer
 from Fan import Fan
@@ -11,8 +11,8 @@ from Fan import Fan
 
 
 def main():
-    cooler = Cooler()
-    t0 = Thermometer(DS18B20(slave="28-000005e94da7"))
+    cooler = Cooler(GPIO=GPIO, input_pin=24)
+    t0 = Thermometer(DS18B20(slave="28-000005e94da7"), rpi=GPIO)
     t1 = Thermometer(DS18B20(slave="28-000006cb82c6"))
 
 
