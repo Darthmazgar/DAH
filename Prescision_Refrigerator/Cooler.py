054db8a0934b40f34bf83cc37298de47853e3bbd
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2018-10-31 13:56:34 +0000

    Hopfuly added a live plotter that will allow us to do other things at the same time.

--- Prescision_Refrigerator/Cooler.py
+++ Prescision_Refrigerator/Cooler.py
@@ -1,4 +1,7 @@
 import time
+import matplotlib.pyplot as plt
+import collections
+import numpy as np
 
 class Cooler(object):
     def __init__(self, GPIO, tmp_aim, high_therm, low_therm, input_pin=24):
@@ -10,6 +13,7 @@ class Cooler(object):
         self.max_on = 60  # Max on time for the cooler in seconds
         self.GPIO.setmode(self.GPIO.BMC)
         self.GPIO.setup(self.ip, GPIO.OUT)  # Set pin as an output
+        self.on = False
 
     def get_tmp_aim(self):
         return self.tmp_aim
@@ -20,11 +24,13 @@ class Cooler(object):
 
     def turn_on(self):
         self.GPIO.output(self.ip, self.GPIO.HIGH)
+        self.on = True
         # time_on =  # Know the on time and off time.
         return True
 
     def turn_off(self):
         self.GPIO.output(self.ip, self.GPIO.LOW)
+        self.on = False
         return False
 
     def converge(self):
@@ -51,6 +57,9 @@ class Cooler(object):
                 conv_time = 20
             time.sleep(conv_time)
 
+            if self.on:
+                time.sleep(5)  # Some rest time
 
             low_tmp = self.low_therm.get_tmp()
+
             high_tmp = self.high_therm.get_tmp()
--- Prescision_Refrigerator/main.py
+++ Prescision_Refrigerator/main.py
@@ -4,6 +4,7 @@ import matplotlib.animation as animation
 import datetime
 import time
 import numpy as np
+import collections
 import RPi.GPIO as GPIO
 from Cooler import Cooler
 from Thermometer import Thermometer
@@ -13,7 +14,7 @@ from Fan import Fan
 
 def main():
     cooler = Cooler(GPIO=GPIO, input_pin=24)
-    high_tmp = Thermometer(DS18B20(slave="28-000005e94da7"), GPIO=GPIO)
+    high_tmp = Thermometer(DS18B20(slave="28-000005e94da7"), GPIO=GPIO)  # Probably need to change high_tmp to the Pelier tmp
     low_tmp = Thermometer(DS18B20(slave="28-000006cb82c6"), GPIO=GPIO)
 
 
