e6e3eecac50dd5fbf4f9ec6da45b89c935dbdcf5
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2018-11-13 18:25:20 +0000

    add smoothing to tmp and reduced smoothing number

--- Prescision_Refrigerator/Thermometer.py
+++ Prescision_Refrigerator/Thermometer.py
@@ -12,7 +12,7 @@ class Thermometer(object):
         self.time_arr = np.arange(arr_len)  # Update with curr time every time the tmp is updated
         self.rate_arr = np.zeros(arr_len)
         self.tmp_aim = tmp_aim
-        self.min_precision = 0.0625  
+        self.min_precision = 0.0625
         self.last_time = 0
 
         if show:
@@ -43,16 +43,16 @@ class Thermometer(object):
         """Return the avg rate over the last range time steps."""
         return np.average(self.rate_arr[-range:])
 
-    def plot_tmp(self, title="", x_lab="", y_lab="", draw=True, smooth=True):
+    def plot_tmp(self, title="", x_lab="", y_lab="", draw=True, smooth=2):
         self.ax1.clear()
         self.ax1.set_title(title)
         self.ax1.set_xlabel(x_lab)
         self.ax1.set_ylabel(y_lab)
-        if smooth:
-            new = np.linspace(self.time_arr[0], self.time_arr[-1], 300)
-            smooth = spline(self.time_arr, self.tmp_arr, new)
-            self.ax1.plot(new, smooth)
-        else:
+        if smooth == 1 or smooth == 2:
+            new = np.linspace(self.time_arr[0], self.time_arr[-1], 150)
+            sm = spline(self.time_arr, self.tmp_arr, new)
+            self.ax1.plot(new, sm)
+        if not smooth or smooth == 2:
             self.ax1.plot(self.time_arr, self.tmp_arr)
         if self.tmp_aim:
             self.ax1.axhline(y=self.tmp_aim, color=(1, 0, 0), linewidth=.8)
@@ -81,7 +81,7 @@ class Thermometer(object):
         self.ax2.set_xlabel(x_lab)
         self.ax2.set_ylabel(y_lab)
         if smooth == 1 or smooth == 2:
-            new = np.linspace(self.time_arr[0], self.time_arr[-1], 300)
+            new = np.linspace(self.time_arr[0], self.time_arr[-1], 150)
             sm = spline(self.time_arr, self.rate_arr, new)
             self.ax2.plot(new, sm)
         if not smooth or smooth == 2:
@@ -94,7 +94,7 @@ class Thermometer(object):
     def conv_score(self, precision, start=0, stop=50):
         ran_low = self.tmp_aim - precision
         ran_high = self.tmp_aim + precision
-        count = 0 
+        count = 0
         for tmp in self.tmp_arr[start:stop]:
             if tmp > ran_high or tmp < ran_low:
                 count += 1
