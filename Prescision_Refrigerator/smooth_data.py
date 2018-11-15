6157808263c7b18b6e2ac25d79e1eb1e27da32d9
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2018-11-15 15:53:25 +0000

    .

--- Prescision_Refrigerator/smooth_data.py
+++ Prescision_Refrigerator/smooth_data.py
@@ -2,14 +2,27 @@ import numpy as np
 from scipy.interpolate import spline
 
 def smooth(data):
-    pass
+    new = np.linspace(self.time_arr[0], self.time_arr[-1], 150)
+    sm = spline(self.time_arr, self.tmp_arr, new)  # Creates smoothed data.
+    self.ax1.plot(new, sm)  # Plots smoothed data.
 
-def read_in_data(file):
-    data = np.genfromtxt(file)
-    print(data)
+def read_in_file(file_n):
+    f = open(file_n, 'r')
+    x_data = []
+    y_data = []
+    for line in f:
+        for char in line:
+            print(char)
+            newstr = oldstr.replace("[", "")
+            newstr = oldstr.replace("]", "")
+            newstr = oldstr.replace(",", "")
+
+
 
 def main():
     data_file = "Cooling_curve_data.txt"
-    read_in_data()
+    data = read_in_file(data_file)
+    # np.split(data)
+    # print(data)
 
 main()
